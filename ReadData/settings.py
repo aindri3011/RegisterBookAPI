83cabe9c71ef7af7c538a15c8f8c27ec1dd60a73
Aindri Mukherjee <77660021+aindri3011@users.noreply.github.com>
2024-06-20 18:30:53 +0530

    Update settings.py

--- ReadData/settings.py
+++ ReadData/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o8r+_wh4)xj78_nsfexq!1(n@1dd8w66f^wbo-+9!zqo5&le1-'
+SECRET_KEY = 'your-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
